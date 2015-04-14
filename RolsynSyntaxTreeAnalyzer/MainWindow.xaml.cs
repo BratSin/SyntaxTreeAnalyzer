0a6fdcbc4ccce45b07aece0c3878f1a60a20c631
BratSin <dpedorenko@mail.ru>
2015-04-14 17:10:38 +0300

    Start

---
+++ RolsynSyntaxTreeAnalyzer/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RolsynSyntaxTreeAnalyzer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoadProject_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog openFileDialog = new Microsoft.Win32.OpenFileDialog();
+
+            openFileDialog.DefaultExt = ".csproj";
+            openFileDialog.Filter = "C# Project (.csproj)|*.csproj";
+            Nullable<bool> result = openFileDialog.ShowDialog();
+
+            if(result == true)
+            {
+                this.txtProjectPath.Text = openFileDialog.FileName;
+            }
+        }
+    }
+}
